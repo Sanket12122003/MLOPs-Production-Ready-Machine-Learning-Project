2d9ba569f1edf0cd8d025b3a5f070bd45628a9df
Sanket12122003 <sanketpawarsp1212@gmail.com>
2024-07-12 23:42:22 +0530

    requrement gathering and adding

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sanket",
+    author_email="sanketpawar12sp@gmail.com",
+    packages=find_packages()
+)
